94cac0d5def0aa2325134ff6e9a55bf949b6493d
Kyle Vedder <kylecvedder@gmail.com>
2015-02-06 17:21:54 -0500

    More work on AI

--- src/kylevedder/com/github/entity/AITankEntity.java
+++ src/kylevedder/com/github/entity/AITankEntity.java
@@ -15,13 +15,15 @@ import kylevedder.com.github.utils.Utils;
 import org.newdawn.slick.Input;
 
 /**
- *Tank Entity
+ * Tank Entity
+ *
  * @author Kyle
  */
 public class AITankEntity extends TankEntity
 {
 
     private AIDriveState driveState = null;
+    private AIDriveState prevDriveState = null;
     private Team enemyTeam = null;
     private float pastTurretUpdateDelta = 0f;
     private final float SHOOT_ANGLE_OFF_THRESHOLD = 0.5f;
@@ -34,6 +36,7 @@ public class AITankEntity extends TankEntity
         this.enemyTeam = enemyTeam;
         pastTurretUpdateDelta = 0f;
         driveState = AIDriveState.DRIVE_TO_TARGET;
+        prevDriveState = AIDriveState.valueOf(driveState.toString());
     }
 
     @Override
@@ -61,6 +64,8 @@ public class AITankEntity extends TankEntity
 
     }
 
+    float drivenDistance = 0f;
+
     /**
      * Updates the driving of the enemy
      *
@@ -69,78 +74,92 @@ public class AITankEntity extends TankEntity
     private void updateDrive(TankEntity target, int delta)
     {
         if (!this.destroyed)
-        {
-            //sets the speed of the vector
-            float tankSpeed = 0;
-
-            float desiredAngle = Utils.calculateAngle(this.hitBox, target.getHitBox());
-            float distToTarget = Utils.getDistanceBetween(this.hitBox, target.getHitBox());
-
-            float deltaAngle = Utils.wrapAngleDelta(desiredAngle - this.vector.getAngle());
-
-            float driveRate = (delta > 0) ? this.getDriveSpeed() / 1000 * delta : 0;
-            switch (this.driveState)
+        {            
+            boolean driveStateSwitched = (driveState != prevDriveState);
+            //System.out.println(driveState.toString() + " " + driveStateSwitched);
+            prevDriveState = AIDriveState.valueOf(driveState.toString());
+            System.out.println("Case when strt: " + driveState);
+            switch (driveState)
             {
                 case DRIVE_TO_TARGET:
 
-                    //if at acceptable angle to drive
-                    if (Math.abs(deltaAngle) <= NO_DRIVE_ANGLE_OFF_THRESHOLD)
+                    //
+                    //Angle Values
+                    //
+                    //calculates angle to closest target
+                    float desiredAngle = Utils.calculateAngle(this.hitBox, target.getHitBox());
+                    //max rate at which the tank may turn
+                    float turnRate = this.getTurnRate() / 1000 * delta;
+                    //Angle amount to add to vector
+                    float deltaAngle = Utils.wrapAngleDelta(desiredAngle - this.vector.getAngle());
+                    //calc maximum allowable angle turn abount
+                    deltaAngle = Utils.clampFloat(deltaAngle, -turnRate, turnRate);
+
+                    //
+                    //Speed/Distance Values
+                    //
+                    //Speed which to drive
+                    float driveRate = this.getDriveSpeed() / 1000 * delta;
+                    //the distance to the closest target
+                    float distToTarget = Utils.getDistanceBetween(this.hitBox, target.getHitBox());
+                    //sets the speed of the vector
+                    float tankSpeed = 0;
+
+                    //if at acceptable angle to drive and not too close to target
+                    if (Math.abs(deltaAngle) <= NO_DRIVE_ANGLE_OFF_THRESHOLD && Math.abs(distToTarget) >= this.DRIVE_TO_DIST)
                     {
-
-                        if (Math.abs(distToTarget) >= this.DRIVE_TO_DIST)//drive to target
-                        {
-                            tankSpeed = driveRate;
-                        }
-
+                        tankSpeed = driveRate;
                     }
 
-                    //regardless of if at acceptable angle to drive, turn toward the target
-                    this.setVector(tankSpeed, desiredAngle, delta);
+                    //may be zero or a value depending on above set speed
+                    this.vector.setSpeed(tankSpeed);
+                    //regardless of if driving, turn toward the target
+                    this.vector.addAngle(deltaAngle);
 
-                    //check to see if vector will collide
+                    //check to see if vector will collide by moving to vector spot
                     CenteredRectangle tenitiveHitbox = new CenteredRectangle(hitBox);
-                    //update tenitiveHitbox to new location
                     tenitiveHitbox.updateDelta(this.vector.getXComp(), this.vector.getYComp(), this.vector.getAngle() - tenitiveHitbox.getAngle());
                     //if collide, go to collide mode
                     if (register.checkCollision(tenitiveHitbox, this.hitBox))
                     {
                         this.driveState = AIDriveState.BACK_AWAY_FROM_COLLISION;
-                        this.vector.setSpeed(0);
                     }
-                    break;                    
-                case BACK_AWAY_FROM_COLLISION:
-                    if (!register.checkCollision(this.hitBox, this.hitBox))
+                    break;
+                case BACK_AWAY_FROM_COLLISION:                    
+                    if (driveStateSwitched)
                     {
+                        drivenDistance = 0f;
+                    }
+                    //Speed which to drive
+                    driveRate = this.getDriveSpeed() / 1000 * delta;
+                    //System.out.println(drivenDistance);
+                    if(drivenDistance < 99f)
+                    {
+                        //clamp the vector to be drive speed or final distance to travel
+                        float toTravel = Utils.clampFloat(100f-drivenDistance, -driveRate, 0);                        
+                        this.vector.setSpeed(toTravel);
+                        drivenDistance += toTravel;
+                        break;
+                    }
+                    else
+                    {
+                        //System.out.println("Setting to drive to target");
                         this.driveState = AIDriveState.DRIVE_TO_TARGET;                        
                     }
+                    
+                    
                     break;
                 case CIRCLE_TARGET:
                     break;
             }
-
+            System.out.println("Case when done: " + driveState);
         }
         else
         {
             this.vector.setSpeed(0);
         }
     }
-
-    /**
-     * Sets the vector and adds the given angle
-     *
-     * @param speed
-     * @param angle
-     * @param delta
-     */
-    private void setVector(float speed, float angle, int delta)
-    {
-        float turnRate = (delta > 0) ? this.getTurnRate() / 1000 * delta : 0;
-        float deltaAngle = Utils.wrapAngleDelta(angle - this.vector.getAngle());
-        deltaAngle = Utils.clampFloat(deltaAngle, -turnRate, turnRate);
-
-        this.vector.setSpeed(speed);
-        this.vector.addAngle(deltaAngle);
-    }
+        
 
     /**
      * Updates the turret to aim and shoot
